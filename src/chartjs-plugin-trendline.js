2bf1865df77d25a7b958ee9ed27ce24352b43d90
Jevgēnijs Zaremba <jevgenijs.zaremba@sonarworks.com>
2024-02-16 15:55:30 +0200

    fix parsing

--- src/chartjs-plugin-trendline.js
+++ src/chartjs-plugin-trendline.js
@@ -55,8 +55,8 @@ const addFitter = (datasetMeta, ctx, dataset, xScale, yScale) => {
 
     const parsing = typeof datasetMeta.controller.chart.options.parsing === "object" ?
         datasetMeta.controller.chart.options.parsing : undefined;
-    const xAxisKey = dataset.trendlineLinear.xAxisKey || parsing ? parsing.xAxisKey : "x";
-    const yAxisKey = dataset.trendlineLinear.yAxisKey || parsing ? parsing.yAxisKey : "y";
+    const xAxisKey = dataset.trendlineLinear.xAxisKey ? dataset.trendlineLinear.xAxisKey : dataset.parsing.xAxisKey ? dataset.parsing.xAxisKey : parsing ? parsing.xAxisKey : "x";
+    const yAxisKey = dataset.trendlineLinear.yAxisKey ? dataset.trendlineLinear.yAxisKey : dataset.parsing.yAxisKey ? dataset.parsing.yAxisKey : parsing ? parsing.yAxisKey : "y";
 
     lineWidth = lineWidth !== undefined ? lineWidth : 3;
 
@@ -245,4 +245,4 @@ if (typeof window !== 'undefined' && window.Chart) {
 // Otherwise, try to export the plugin
 try {
     module.exports = exports = pluginTrendlineLinear;
-} catch (e) { }
+} catch (e) {}
